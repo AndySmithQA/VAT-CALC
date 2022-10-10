78b21bfa6b336324ac9602dc15f194172c1c959e
AndySmithQA <andy.smtih@qa.com>
2022-10-10 13:05:42 +0100

    basic 1.2

--- vat/src/App.js
+++ vat/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PriceEntryField from './PriceEntryField';
 
 function App() {
   return (
     <div className="App">
       Vat Calculator
+      <PriceEntryField label="Price Excluding VAT: " />
+      <PriceEntryField label="Price Including VAT: " />
     </div>
   );
 }
